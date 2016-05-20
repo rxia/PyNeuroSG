bfa7e8480485951316dea64829beb5c4ed993ade
SummitKwan <SummitKwan@hotmail.com>
2016-05-20 01:40:01 -0400

    Standardize dg2df, add test case of newly recorded data, test documentation system

--- dg2df.py
+++ dg2df.py
@@ -1,11 +1,40 @@
-# DLSH dg (dynamic group) to python pandas df (DataFrame)
+# convert DLSH dg (dynamic group) to python pandas df (DataFrame)
 # Shaobo Guan, 2016-0517 TUE
 
 import os
 import sys
 cur_path = os.path.dirname(__file__)  # get the path of this file
-sys.path.append( os.path.join(cur_path, 'dgread'))
+sys.path.append(os.path.join(cur_path, 'dgread'))
 
 import dgread
 import pandas as pd
 from scipy import stats
+
+def dg2df(dgfile):
+    data_dg = dgread.dgread(dgfile)
+
+    remove_short_columns(data_dg)
+
+    data_df = pd.DataFrame.from_dict(data_dg)
+
+    return data_df
+
+
+def remove_short_columns(data_dg, f_verbose=False):
+    # ===== remove irregular columns of dg, make it ready to be converted to pandas df =====
+    # ----- get number of rows by majority vote of all columns -----
+    dg_keys = data_dg.keys()
+    num_rows = []  # a list of number of rows for every column
+    for dg_key in dg_keys:
+        num_rows.append(len(data_dg[dg_key]))
+        if f_verbose:
+            print('filed {} has {} rows'.format(dg_key, len(data_dg[dg_key]) ))
+    N = stats.mode(num_rows)[0][0]  # get number of rows by majority vote
+
+    # ----- remove columns with different numbers of rows -----
+    for dg_key in dg_keys:
+        if len(data_dg[dg_key]) != N:
+            if f_verbose:
+                print('remove dg column: {} that contains {} rows'.format(dg_key, len(data_dg[dg_key]) ))
+            del data_dg[dg_key]
+
--- test_dgread.py
+++ test_dgread.py
@@ -1,42 +1,34 @@
 # test DLSH dgread function
 # from test_dgread import *
 
-# set path
 import os
 import sys
-cur_path = os.path.dirname(__file__)  # get the path of this file
-sys.path.append( os.path.join(cur_path, 'dgread'))
-
-import dgread
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-from scipy import stats
+pd.set_option('display.max_columns', None)
 
-data_dg = dgread.dgread('/Volumes/Labfiles/tmp/d_MTS_bin_051116.dgz')
-
-dg_keys = data_dg.keys()
-
-
-def remove_short_columns(data_dg, f_verbose=False):
-
-    # ----- get number of rows by majority vote of all columns -----
-    num_rows = []  # a list of number of rows for every column
-    for dg_key in dg_keys:
-        num_rows.append(len(data_dg[dg_key]))
-        if f_verbose:
-            print('filed {} has {} rows'.format(dg_key, len(data_dg[dg_key]) ))
-    N = stats.mode(num_rows)[0][0]  # get number of rows by majority vote
-
-    # ----- remove columns with different numbers of rows -----
-    for dg_key in dg_keys:
-        if len(data_dg[dg_key]) != N:
-            if f_verbose:
-                print('remove dg column: {} that contains {} rows'.format(dg_key, len(data_dg[dg_key]) ))
-            del data_dg[dg_key]
+# import the customize module
+cur_path = os.path.dirname(__file__)  # get the path of this file
+sys.path.append(cur_path)
+import dg2df
 
 
-remove_short_columns(data_dg)
-data_df = pd.DataFrame.from_dict(data_dg)
+# set path of datafile
+dir_dg  = '/Volumes/Labfiles/projects/analysis/shaobo/data_dg'
+file_dg = 'd_MTS_bin_051916009.dg'
+path_dg = os.path.join(dir_dg, file_dg)
+
+# load file
+data_df = dg2df.dg2df(path_dg)
+
+# save df to disk
 # data_df.to_hdf('test_hdf_store.h5', 'stimdg', mode='w')
-data_df.to_json('test_hdf_store.json')
+# data_df.to_json('test_json_store.json')
+
+# analyze
+data_df.groupby('NoiseOpacity')[['status']].agg(np.mean).plot()
+
+
 
 print("finish")
