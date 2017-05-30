b53779859cd595f933eaecea78acd920ffca3f8c
SummitKwan <SummitKwan@hotmail.com>
2017-05-30 16:28:50 -0400

    script to plot the depth profile when the electrode is retraced at a constanst speed, useful to infer the location of elctrode

--- script_2017-0526_electrode_tract_rebuild.py
+++ script_2017-0526_electrode_tract_rebuild.py
@@ -52,9 +52,9 @@ def load_blk(keyword_blk=keyword_blk, keyword_tank=keyword_tank, dir_tdt_tank=di
     blk = data_load_DLSH.standardize_blk(blk)
     return blk.segments[0]
 
-def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_tank, ch_index = 1,
-                                 speed_retract=speed_retract, binsize_dist=binsize_dist,
-                                 tf_reverse_dist=tf_reverse_dist,
+def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_tank, ch_index = 1, range_ch= None,
+                                 speed_retract=speed_retract, binsize_dist=binsize_dist, range_ch_pos_rel=None,
+                                 tf_reverse_dist=tf_reverse_dist, total_distance=None,
                                  tf_spectragram=tf_spectragram, tf_spectragram_interactive=tf_spectragram_interactive):
     """ generate plot of depth profile of spiking pattern and lfp spectrogram
 
@@ -65,6 +65,8 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
 
     """ ----- use time and speed to infer depth ----- """
     t_range = np.array([seg.t_start, seg.t_stop])
+    if total_distance is not None:
+        t_range[1] = total_distance/speed_retract
     dist_range = t_range*speed_retract
 
     """ ----- get spiking pattern ----- """
@@ -72,7 +74,18 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
     spk_dist = spk_time * speed_retract
     spk_bin, dist_bin_edge = np.histogram(spk_dist, np.arange(dist_range[0], dist_range[1], binsize_dist))
     dist_bin_center = (dist_bin_edge[:-1] + dist_bin_edge[1:]) / 2
-
+    if range_ch is not None:
+        list_spk_bin = []
+        for ch_cur in range_ch:
+            try:
+                spk_time_cur = np.sort(np.concatenate([np.array(spktrn.times) for spktrn in seg.spiketrains if
+                                               spktrn.annotations['channel_index'] == ch_cur]))
+            except:
+                spk_time_cur = np.array([])
+            spk_dist_cur = spk_time_cur * speed_retract
+            spk_bin_cur, _ = np.histogram(spk_dist_cur, np.arange(dist_range[0], dist_range[1], binsize_dist))
+            list_spk_bin.append(spk_bin_cur)
+        spk_bin_array = np.vstack(list_spk_bin)
 
     """ ----- compute spectrogram ----- """
     if tf_spectragram:
@@ -80,8 +93,10 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
         lfp_sig = [anasig for anasig in seg.analogsignals if re.match('LFPs.*', anasig.name) and anasig.annotations['channel_index']==ch_index][0]
         lfp_trace = np.ravel(lfp_sig) * scale_factor
         lfp_fs = np.array(lfp_sig.sampling_rate)
-        lfp_ts = lfp_fs * range(len(lfp_trace)) + np.array(lfp_sig.t_start)
-        lfp_dist = lfp_ts* speed_retract
+        lfp_ts = range(len(lfp_trace))/lfp_fs + np.array(lfp_sig.t_start)
+        if total_distance is not None:
+            lfp_ts = lfp_ts[lfp_ts<t_range[1]]
+            lfp_trace = lfp_trace[:len(lfp_ts)]
         spcg, spcg_t, spcg_f = pna.ComputeSpectrogram(data=np.expand_dims(lfp_trace, axis=0),
                                fs=lfp_fs, t_ini=lfp_ts[0], t_bin=1.0, t_step=1.0, t_axis=1, f_lim=[0,100])
         spcg_dist = spcg_t * speed_retract
@@ -89,10 +104,12 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
     """ ----- direction of distance axis ----- """
     if tf_reverse_dist:   # top to bottom
         dist_bin = np.flip(dist_bin_center, axis=0)
-        dist_spcg = np.flip(spcg_dist, axis=0)
+        if tf_spectragram:
+            dist_spcg = np.flip(spcg_dist, axis=0)
     else:                 # bottom to top
         dist_bin = dist_bin_center
-        dist_spcg = spcg_dist
+        if tf_spectragram:
+            dist_spcg = spcg_dist
 
     """ ----- plot ----- """
     if tf_spectragram:
@@ -102,7 +119,15 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
 
     """ upper panel: spiking pattern """
     plt.axes(h_ax[0,0])
-    plt.plot(dist_bin, spk_bin)
+    if range_ch is not None:
+        list_color = plt.cm.rainbow(np.linspace(0,1,len(range_ch)))
+        for i, ch_cur in enumerate(range_ch):
+            if range_ch_pos_rel is None:
+                dist_shift = 0
+            else:
+                dist_shift = range_ch_pos_rel[i]
+            plt.plot(dist_bin+dist_shift, spk_bin_array[i,:], color=list_color[i], linewidth=1, alpha=0.5)
+    plt.plot(dist_bin, spk_bin, color='k', linewidth=2)
     plt.xlabel('distance, um')
     plt.ylabel('spk count in {} um window'.format(binsize_dist))
     plt.title('spiking pattern')
@@ -140,16 +165,33 @@ def plot_electrode_depth_profile(keyword_blk=keyword_blk, keyword_tank=keyword_t
             def savefig(event):
                 plt.savefig('{}/electrode_depth_profile_tank_{}.png'.format(path_save_fig, keyword_tank))
             h_button_savefig.on_clicked(savefig)
-
+    h_ax[0,0].set_xlim([dist_bin.min(), dist_bin.max()])
     plt.suptitle('electrode depth profile, tank {}'.format(keyword_tank))
 
     """ return results """
     return_dict = {'spk': (dist_bin, spk_bin), 'spcg': None, 'slider': None}
     if tf_spectragram:
         return_dict['spcg'] = (dist_spcg, spcg_f, spcg[0, :, :])
-    if tf_spectragram_interactive:    # to return the widegets object is important, otherwise widgets do not work whtin functions
+    if tf_spectragram_interactive and tf_spectragram:    # to return the widegets object is important, otherwise widgets do not work whtin functions
         return_dict['widgets'] = (h_slider_clim_min, h_slider_clim_max, h_button_savefig)
 
     return return_dict
 
+""" ----- process data day by day ----- """
 plot_electrode_depth_profile(keyword_tank = 'Murphy-170526-110333', keyword_blk = 'retract')
+plot_electrode_depth_profile(keyword_tank = '.*160810.*', keyword_blk = '.*retreat.*', ch_index=16, range_ch=np.arange(1, 16+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*160817.*', keyword_blk = '.*retreat.*', ch_index=16, range_ch=np.arange(1, 16+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*160902.*', keyword_blk = '.*retreat.*', ch_index=16, range_ch=np.arange(1, 16+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*160904.*', keyword_blk = '.*retreat.*', ch_index=16, range_ch=np.arange(1, 16+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161015.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161023.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161026.*', keyword_blk = '.*retreat.*', ch_index=40, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161118.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100), total_distance=13800)
+plot_electrode_depth_profile(keyword_tank = '.*161121.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161125.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161202.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161206.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*161228.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*170103.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+plot_electrode_depth_profile(keyword_tank = '.*170221.*', keyword_blk = '.*retreat.*', ch_index=48, range_ch=np.arange(33, 48+1), range_ch_pos_rel=np.arange(-1500,1,100))
+
