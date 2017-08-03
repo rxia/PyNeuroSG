b48dba0791faf9458918bea717e47301b08dc5e4
ryanlloydmiller <ryanlloydmiller@gmail.com>
2017-08-03 14:31:38 -0400

    added consecutive analysis in 0803

--- script/script_2017-0716_abe_dg_plot.py
+++ script/script_2017-0716_abe_dg_plot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import numpy as np
 import scipy as sp
 import pandas as pd         # pandas tabular DataFrame for task/behavioral data
@@ -7,7 +8,6 @@ import matplotlib as mpl    # plot
 import matplotlib.pyplot as plt
 import re                   # regular expression
 import warnings
-import misc_tools
 
 
 # costom packages
@@ -17,58 +17,32 @@ import PyNeuroAna as pna
 import PyNeuroPlot as pnp
 import data_load_DLSH
 
-
 """ get dg files and sort by time """
 
 # dir_dg = '/shared/homes/sguan/neuro_data/stim_dg'
-dir_dg = '/shared/lab/projects/analysis/ryan/data_dg'
+#dir_dg = '/shared/lab/projects/analysis/ryan/data_dg'
+dir_dg = 'L:/projects/analysis/ryan/data_dg'
 list_name_dg_all = os.listdir(dir_dg)
 
-keyword_dg = 'h.*_072117.*'
+keyword_dg = 'a.*_072117.*'
 
 _, data_df, name_datafiles = data_load_DLSH.load_data(keyword=keyword_dg, tf_interactive=True, dir_dg=dir_dg, mode='dg')
 data_df = data_load_DLSH.standardize_data_df(data_df)
-filename_common = misc_tools.str_common(name_datafiles)
-data_df['RT'] = data_df['rts'] - data_df['TargetOnset']
+
+data_df['truert'] = data_df['rts']-data_df['TargetOnset']
 
 resp_rate = 1.0*len(data_df)/data_df['obs_total'][0]
+reload(pnp); pnp.DfPlot(data_df[data_df['side']<0.5], values='response', x='TargetOnset', plot_type='box')
 
-""" use DfPlot """
-# pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='side', p='fileindex', plot_type='bar', errbar='se')
-pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='side', plot_type='box', title_text='{}, resp_rate={:.2f}'.format(filename_common, resp_rate))
-plt.savefig('./temp_figs/RT_{}.png'.format(filename_common))
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['obsid'], plot_type='dot')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['obsid'], c=data_df['filename'], plot_type='dot')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['obsid'], p=data_df['filename'], plot_type='dot')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['obsid'], c=data_df['TargetOnset'], p=data_df['filename'], plot_type='dot')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['obsid'], c=data_df['TargetOnset'], p=data_df['filename'], plot_type='dot', tf_legend=True, values_name='rt', c_name='TargetOnset', p_name='filename')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['TargetOnset'], c=data_df['side'], p=data_df['filename'], plot_type='bar', tf_legend=True, values_name='rt', c_name='side', p_name='filename')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['TargetOnset'], c=data_df['side'], p=data_df['filename'], plot_type='box', tf_legend=True, values_name='rt', c_name='side', p_name='filename')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['TargetOnset'], c=data_df['side'], p=data_df['filename'], plot_type='box', tf_legend=True, values_name='rt', c_name='side', p_name='filename')
+reload(pnp); pnp.GroupPlot(values=data_df['rts'], x=data_df['TargetOnset'], c=data_df['side'], plot_type='violin', tf_legend=True, values_name='rt', c_name='side')
+reload(pnp); pnp.GroupPlot(values=data_df['truert'], x=data_df['TargetOnset'],  plot_type='box', tf_legend=True, values_name='rt', c_name='side')
+reload(pnp); pnp.GroupPlot(values=data_df['truert'], x=data_df['TargetOnset'], c=data_df['side'],p=data_df['filename'], limit=(data_df['status']==1), plot_type='bar', tf_legend=True, values_name='rt', c_name='side')
 
-
-
-""" train left/right switch """
-keyword_dg = 'h.*_071917.*'
-
-_, data_df, name_datafiles = data_load_DLSH.load_data(keyword=keyword_dg, tf_interactive=True, dir_dg=dir_dg, mode='dg')
-data_df = data_load_DLSH.standardize_data_df(data_df)
-filename_common = misc_tools.str_common(name_datafiles)
-data_df['RT'] = data_df['rts'] - data_df['TargetOnset']
-
-resp_rate = 1.0*len(data_df)/data_df['obs_total'][0]
-title_text = '{}, resp_rate={:.2f}'.format(filename_common, resp_rate)
-pnp.DfPlot(data_df, values='status', x='file', c='side', plot_type='bar', title_text=title_text)
-pnp.DfPlot(data_df, values='RT', x='file', c='side', plot_type='box', title_text='{}, resp_rate={:.2f}'.format(filename_common, resp_rate))
-pnp.DfPlot(data_df, values='RT', x='side', c='status', plot_type='violin', title_text='{}, resp_rate={:.2f}'.format(filename_common, resp_rate))
-
-
-reload(pnp); pnp.DfPlot(data_df, values='status', x='file', c='side', title_text=title_text)
-reload(pnp); pnp.DfPlot(data_df, values='RT', x='file', c='side', title_text=title_text)
-reload(pnp); pnp.DfPlot(data_df, values='status', x='TargetOnset', c='side', title_text=title_text)
-reload(pnp); pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='status', p='side', title_text=title_text)
-
-
-
-""" train left/right switch """
-keyword_dg = 'h.*_072017.*'
-
-_, data_df, name_datafiles = data_load_DLSH.load_data(keyword=keyword_dg, tf_interactive=True, dir_dg=dir_dg, mode='dg')
-data_df = data_load_DLSH.standardize_data_df(data_df)
-filename_common = misc_tools.str_common(name_datafiles)
-data_df['RT'] = data_df['rts'] - data_df['TargetOnset']
-
-reload(pnp); pnp.DfPlot(data_df, values='status', x='TargetOnset', c='side', title_text=title_text)
-reload(pnp); pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='status', p='side', title_text=title_text)
--- script/script_2017-0803_abe_dg_plot.py
+++ script/script_2017-0803_abe_dg_plot.py
@@ -1,3 +1,42 @@
+import io
+import matplotlib.pyplot as plt
+from PySide.QtGui import QApplication, QImage
+
+import sys
+app = QApplication(sys.argv)
+
+def add_clipboard_to_figures():
+    # use monkey-patching to replace the original plt.figure() function with
+    # our own, which supports clipboard-copying
+    oldfig = plt.figure
+
+    def newfig(*args, **kwargs):
+        fig = oldfig(*args, **kwargs)
+        def clipboard_handler(event):
+            if event.key == 'ctrl+c':
+                # store the image in a buffer using savefig(), this has the
+                # advantage of applying all the default savefig parameters
+                # such as background color; those would be ignored if you simply
+                # grab the canvas using Qt
+                buf = io.BytesIO()
+                fig.savefig(buf)
+
+                blah = QImage.fromData(buf.getvalue())
+
+                QApplication.clipboard().setImage(blah)
+                buf.close()
+
+        fig.canvas.mpl_connect('key_press_event', clipboard_handler)
+        return fig
+
+    plt.figure = newfig
+
+add_clipboard_to_figures()
+
+
+
+
+
 import os
 import sys
 import numpy as np
@@ -33,7 +72,8 @@ def order_consecutive(x):
 """ get dg files and sort by time """
 
 # dir_dg = '/shared/homes/sguan/neuro_data/stim_dg'
-dir_dg = '/shared/lab/projects/analysis/ryan/data_dg'
+# dir_dg = '/shared/lab/projects/analysis/ryan/data_dg'
+dir_dg = 'L:/projects/analysis/ryan/data_dg'
 list_name_dg_all = os.listdir(dir_dg)
 
 keyword_dg = 'a.*_080317.*'
@@ -48,7 +88,7 @@ resp_rate = 1.0*len(data_df)/data_df['obs_total'][0]
 """ use DfPlot """
 # pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='side', p='fileindex', plot_type='bar', errbar='se')
 pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='side', plot_type='box', title_text='{}, resp_rate={:.2f}'.format(filename_common, resp_rate))
-plt.savefig('./temp_figs/RT_{}.png'.format(filename_common))
+
 
 
 
@@ -149,7 +189,7 @@ pnp.DfPlot(data_df, values='status', x='TargetOnset', c='side', title_text=title
 pnp.DfPlot(data_df, values='RT', x='TargetOnset', c='resp', p='status', title_text=title_text)
 
 
-pnp.DfPlot(data_df, values='status', x='order_consecutive', c='side', limit=(data_df['order_consecutive']<6)*(data_df['file']>=9), title_text=title_text)
+pnp.DfPlot(data_df, values='status', x='order_consecutive', c='side', limit=(data_df['order_consecutive']<6)*(data_df['file']>=10), title_text=title_text)
 plt.savefig('./temp_figs/status_consecutive_{}.png'.format(filename_common))
 pnp.DfPlot(data_df, values='RT', x='order_consecutive', c='side', limit=(data_df['order_consecutive']<6)*(data_df['file']>=9), title_text=title_text)
 plt.savefig('./temp_figs/RT_consecutive_{}.png'.format(filename_common))
